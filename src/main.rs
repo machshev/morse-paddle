232b04187638e71bf23f8b1cd01beb3778fae329
David James McCorrie <djmccorrie@gmail.com>
2026-01-29 23:37:23 +0000

    feat: add support for small OLED display
    Signed-off-by: David James McCorrie <djmccorrie@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,25 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::{
+    bind_interrupts,
     gpio::{Input, Level, Output, OutputType, Pull, Speed},
+    i2c::{self},
+    peripherals,
     time::khz,
     timer::simple_pwm::{PwmPin, SimplePwm},
 };
 use embassy_time::Timer;
 use morse_paddle::{IambicMode, Keyer, PaddleInput, send_element};
+use ssd1306::{I2CDisplayInterface, Ssd1306Async, prelude::*};
 use {defmt_rtt as _, panic_probe as _};
 
 const WPM: u64 = 15;
 const UNIT_MS: u64 = 1200 / WPM;
+
+bind_interrupts!(struct Irqs {
+    I2C1_EV => i2c::EventInterruptHandler<peripherals::I2C1>;
+    I2C1_ER => i2c::ErrorInterruptHandler<peripherals::I2C1>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -42,6 +51,24 @@ async fn main(_spawner: Spawner) {
     let mut buzzer_pass = pwm.ch1();
     buzzer_pass.enable();
     buzzer_pass.set_duty_cycle_fully_off();
+
+    let i2c = i2c::I2c::new(
+        p.I2C1,
+        p.PB6,
+        p.PB7,
+        Irqs,
+        p.DMA1_CH6,
+        p.DMA1_CH7,
+        Default::default(),
+    );
+
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306Async::new(interface, DisplaySize128x32, DisplayRotation::Rotate0)
+        .into_terminal_mode();
+    display.init().await.unwrap();
+    let _ = display.clear().await;
+
+    let _ = display.write_str("Hello").await;
 
     info!("Iambic Mode B keyer ready –  {} WPM", WPM);
 
